cbf29b9a1ea5420cd4a2d9b7d07d304064323038
dokclayton <133478498+dokclayton@users.noreply.github.com>
2023-05-29 20:27:28 +0800

    Add files via upload

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+const greeting = document.querySelector('.greeting');
+
+window.onload = () => {
+    if(!sessionStorage.name){
+        location.href = '/login';
+    } else{
+        greeting.innerHTML = `hello ${sessionStorage.name}!`;
+    }
+}
+
+const logOut = document.querySelector('.logout');
+
+logOut.onclick = () => {
+    sessionStorage.clear();
+    location.reload();
+}
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.add("hidden");
+}
+  
